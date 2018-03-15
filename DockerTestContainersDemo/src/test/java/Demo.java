99a1098cefc1fcb5ff1532e0f2aae1eb1470de5d
endless-qa <camucenko@gmail.com>
2018-03-15 21:24:27 +0200

    Added Docker-TestContainers-Jenkins project

---
+++ DockerTestContainersDemo/src/test/java/Demo.java
@@ -0,0 +1,20 @@
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+
+public class Demo {
+
+    @Rule
+    public BrowserWebDriverContainer chrome = new BrowserWebDriverContainer().
+            withDesiredCapabilities(DesiredCapabilities.chrome());
+
+    @Test
+    public void demoTest() {
+        RemoteWebDriver driver = chrome.getWebDriver();
+
+        driver.get("http://amazon.com");
+        System.out.println(driver.getTitle());
+    }
+}
